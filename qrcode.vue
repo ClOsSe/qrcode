e4d779e7a2ef36cb10c292f0596202449c1f286d
ClOsSe <44271832+ClOsSe@users.noreply.github.com>
2021-10-18 05:44:17 -0700

    create a project and
    run npm i qrcode  and then replace the file

---
+++ qrcode.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="col-12 center" align="center">
+        <div id="qrcode-container">
+            <canvas style="width:150px;height:150px" id="canvas"></canvas>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import QRCode from 'qrcode'
+
+@Component({
+    components:{}
+})
+export default class Qrcode extends Vue{
+    @Prop({default:()=>{return '';}}) url:string;
+    mounted(){
+        if (this.url != '') {
+            QRCode.toCanvas(document.getElementById('canvas'), this.url, function (error:any) {
+                if (error) console.error(error)
+            console.log('success!');
+            })
+        }
+    }
+}
+</script>
